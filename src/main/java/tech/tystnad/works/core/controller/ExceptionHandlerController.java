89cd6451689a509eb505fe3ab1fffa45d32c1c90
L <33799701+StilleMenschen@users.noreply.github.com>
2020-09-16 11:10:28 +0800

    1600225815882

--- src/main/java/tech/tystnad/works/core/controller/ExceptionHandlerController.java
+++ src/main/java/tech/tystnad/works/core/controller/ExceptionHandlerController.java
@@ -5,11 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import tech.tystnad.works.core.exception.BusinessException;
 import tech.tystnad.works.model.ResponseObjectEntity;
 
@@ -28,7 +31,7 @@ public class ExceptionHandlerController {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseObjectEntity<Object> dataValidatorHandler(MethodArgumentNotValidException e) {
+    public ResponseObjectEntity<Object> dataValidator(MethodArgumentNotValidException e) {
         // 获取第一个错误
         FieldError error = e.getBindingResult().getFieldErrors().get(0);
         String message = error.getDefaultMessage();
@@ -48,12 +51,13 @@ public class ExceptionHandlerController {
 
     /**
      * 数据库访问错误
+     *
      * @param e
      * @return
      */
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({SQLException.class, DataAccessException.class})
-    public ResponseObjectEntity<Object> sqlHandler(SQLException e) {
+    public ResponseObjectEntity<Object> dataAccess(SQLException e) {
         logger.error(e.toString(), e);
         ResponseObjectEntity<Object> entity = new ResponseObjectEntity<>();
         entity.setCode(400);
@@ -63,12 +67,13 @@ public class ExceptionHandlerController {
 
     /**
      * 业务异常
+     *
      * @param e
      * @return
      */
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(BusinessException.class)
-    public ResponseObjectEntity<Object> businessHandler(BusinessException e) {
+    public ResponseObjectEntity<Object> business(BusinessException e) {
         logger.error(e.toString(), e);
         ResponseObjectEntity<Object> entity = new ResponseObjectEntity<>();
         entity.setCode(-1);
@@ -77,7 +82,56 @@ public class ExceptionHandlerController {
     }
 
     /**
-     * 通用错误异常,当spring检测不到具体的ExceptionHandler时,才会默认使用这个ExceptionHandler进行处理
+     * 不支持的请求方式
+     *
+     * @param e
+     * @return
+     */
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseObjectEntity<Object> methodNotSupported(HttpRequestMethodNotSupportedException e) {
+        logger.error(e.toString(), e);
+        ResponseObjectEntity<Object> entity = new ResponseObjectEntity<>();
+        entity.setCode(HttpStatus.METHOD_NOT_ALLOWED.value());
+        entity.setMsg(e.getMessage());
+        return entity;
+    }
+
+    /**
+     * 不支持的媒体类型
+     *
+     * @param e
+     * @return
+     */
+    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+    @ExceptionHandler(HttpMediaTypeException.class)
+    public ResponseObjectEntity<Object> mediaTypeUnsupported(HttpMediaTypeException e) {
+        logger.error(e.toString(), e);
+        ResponseObjectEntity<Object> entity = new ResponseObjectEntity<>();
+        entity.setCode(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value());
+        entity.setMsg(e.getMessage());
+        return entity;
+    }
+
+    /**
+     * 请求体大小超过服务器限制
+     *
+     * @param e
+     * @return
+     */
+    @ResponseStatus(HttpStatus.PAYLOAD_TOO_LARGE)
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseObjectEntity<Object> payloadTooLarge(MaxUploadSizeExceededException e) {
+        logger.error(e.toString(), e);
+        ResponseObjectEntity<Object> entity = new ResponseObjectEntity<>();
+        entity.setCode(HttpStatus.PAYLOAD_TOO_LARGE.value());
+        entity.setMsg(e.getMessage());
+        return entity;
+    }
+
+    /**
+     * 通用错误异常,当spring检测不到具体的ExceptionHandler时,才默认使用这个ExceptionHandler进行处理
+     *
      * @param e
      * @return
      */
