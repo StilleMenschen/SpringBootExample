db72f407f50d13d0bee25895987dd7dae66bfb07
Stillemenschen <2956686701@qq.com>
2022-06-19 11:29:17 +0800

    feat: support form based authentication

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -15,6 +15,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.example.demo.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.example.demo.security.ApplicationUserRole.*;
@@ -35,20 +38,20 @@ public class ApplicationSecurityConfig {
     protected UserDetailsService userDetailsService() {
         UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
-                .password(passwordEncoder.encode("abc123"))
+                .password(passwordEncoder.encode("pwd123"))
 //                .roles(STUDENT.name()) // ROLE_STUDENT
                 .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
         UserDetails lindaUser = User.builder()
                 .username("linda")
-                .password(passwordEncoder.encode("abc123456"))
+                .password(passwordEncoder.encode("pwd1234"))
 //                .roles(ADMIN.name()) // ROLE_ADMIN
                 .authorities(ADMIN.getGrantedAuthorities())
                 .build();
         UserDetails tomUser = User.builder()
                 .username("tom")
-                .password(passwordEncoder.encode("abc123456"))
+                .password(passwordEncoder.encode("pwd1234"))
 //                .roles(ADMIN_TRAINEE.name()) // ROLE_ADMIN
                 .authorities(ADMIN_TRAINEE.getGrantedAuthorities())
                 .build();
@@ -60,9 +63,11 @@ public class ApplicationSecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and() // Cross-site Request Forgery 跨站请求伪造, 如果接口不是为浏览器提供的, 可以禁用此功能
+                // Cross-site Request Forgery 跨站请求伪造, 如果接口不是为浏览器提供的, 可以禁用此功能
+//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/", "/login", "/logout", "/index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasAnyRole(STUDENT.name())
 //                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermission())
 //                .antMatchers(HttpMethod.POST, "/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermission())
@@ -70,8 +75,21 @@ public class ApplicationSecurityConfig {
 //                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN_TRAINEE.name(), ADMIN.name())
                 .anyRequest()
                 .authenticated()
-                // .and().userDetailsService(userDetailsService())
-                .and().httpBasic().and().build();
+//              .and().httpBasic() // 基于 HTTP 的基础验证, 需要在请求头 Authorization 中包含 BASE64 加密的用户名密码
+                .and()
+                // 基于 FORM 表单的基础验证, 需要先使用用户名密码登录, 登录信息缓存在会话中
+                .formLogin().loginPage("/login")
+                .defaultSuccessUrl("/courses", Boolean.TRUE)
+                .usernameParameter("username").passwordParameter("password")
+                .and()
+                // 记住用户信息 7 天内不用重复登录
+                .rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(7)).key("something")
+                .rememberMeParameter("remember-me")
+                .and().logout().logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                .clearAuthentication(Boolean.TRUE).invalidateHttpSession(Boolean.TRUE)
+                .deleteCookies("JSESSIONID").logoutSuccessUrl("/login")
+                .and().build();
     }
 //    @Bean
 //    public WebSecurityCustomizer webSecurityCustomizer() {
