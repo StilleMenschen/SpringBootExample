c654b0c7b6e9713321b67ca81d4b3daebccc8748
Stillemenschen <2956686701@qq.com>
2022-06-23 09:01:45 +0800

    feat: add user and role api

--- src/main/java/com/example/demo/api/UserResource.java
+++ src/main/java/com/example/demo/api/UserResource.java
@@ -1,13 +1,15 @@
 package com.example.demo.api;
 
+import com.example.demo.domain.Role;
+import com.example.demo.domain.RoleToUserForm;
 import com.example.demo.domain.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,5 +25,25 @@ public class UserResource {
     @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok(userService.getUsers());
+    }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        final URI uri = URI.create(ServletUriComponentsBuilder
+                .fromCurrentRequestUri().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        final URI uri = URI.create(ServletUriComponentsBuilder
+                .fromCurrentRequestUri().path("/api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form) {
+        userService.addRoleToUser(form.getUsername(), form.getRoleName());
+        return ResponseEntity.ok().build();
     }
 }
