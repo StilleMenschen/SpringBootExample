3c1b719102b8f9a94870d035963a5f796a0e88ef
Stillemenschen <2956686701@qq.com>
2022-06-26 18:14:11 +0800

    chore: change add user command line bean name

--- src/main/java/com/example/demo/ExampleDemoApplication.java
+++ src/main/java/com/example/demo/ExampleDemoApplication.java
@@ -16,26 +16,4 @@ public class ExampleDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExampleDemoApplication.class, args);
     }
-
-    @Bean(name = "cmd2")
-    public CommandLineRunner commandLineRunner(UserService userService) {
-        return args -> {
-            userService.saveRole(new Role(null, "ROLE_USER"));
-            userService.saveRole(new Role(null, "ROLE_MANAGER"));
-            userService.saveRole(new Role(null, "ROLE_ADMIN"));
-            userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
-
-            userService.saveUser(new User(null, "John Travolta", "john", "1234", Collections.emptyList()));
-            userService.saveUser(new User(null, "Will Smith", "will", "1234", Collections.emptyList()));
-            userService.saveUser(new User(null, "Jim Carry", "jim", "1234", Collections.emptyList()));
-            userService.saveUser(new User(null, "Arnold Schwarzenegger", "arnold", "1234", Collections.emptyList()));
-
-            userService.addRoleToUser("john", "ROLE_USER");
-            userService.addRoleToUser("will", "ROLE_MANAGER");
-            userService.addRoleToUser("jim", "ROLE_SUPER_ADMIN");
-            userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
-            userService.addRoleToUser("arnold", "ROLE_ADMIN");
-            userService.addRoleToUser("arnold", "ROLE_USER");
-        };
-    }
 }
